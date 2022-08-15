e72a559c651cf17a9a864712d77162b44b57ffff
djbetamax <hugots23@gmail.com>
2022-08-15 20:54:42 -0300

    added: a way to post an updated message in chat

--- inspiration-mechanic/give-inspiration.js
+++ inspiration-mechanic/give-inspiration.js
@@ -9,20 +9,21 @@ main()
       const inspecActor = actor.items.find( inspecActor => inspecActor.data.name == item.name);
         if ( inspecActor == null || inspecActor == undefined ) {
           await actor.createEmbeddedDocuments('Item', [ item.toObject() ])
+            ChatMessage.create ({
+              speaker: { alias: 'Dungeon Master' },
+              content: '<h2>Give Inspiration</h2>' + `<h3> ${actor.name} its recieving an @Item[Inspiração].</h3> <p>Have 1 of a 3 maximum!</p> [[/r 1d6]]`
+            })
         } else {
             await inspecActor.update( { 'data.qtd': inspecActor.data.data.qtd + 1 } )
-              if ( inspecActor.data.data.qtd > 3 || inspecActor.data.data.qtd == 4 ) {
-                await inspecActor.update ( { 'data.qtd': inspecActor.data.data.qtd - 1 } )
-                ui.notifications.error('This player reached at the maximum Inspiration number.')
+            if ( inspecActor.data.data.qtd > 3 || inspecActor.data.data.qtd == 4 ) {
+              await inspecActor.update ( { 'data.qtd': inspecActor.data.data.qtd - 1 } )
+              ui.notifications.error('This player reached at the maximum Inspiration number.')
+            } else {
+                ChatMessage.create ({
+                  speaker: { alias: 'Dungeon Master' },
+                  content: '<h2>Give Inspiration</h2>' + `<h3> ${actor.name} its recieving an @Item[Inspiração].</h3> <p>Have ${inspecActor.data.data.qtd} of a 3 maximum!</p> [[/r 1d6]]`
+                })
               }
           }
         }
       }
-      // TODO: discover where put this code under
-    // let getActorName = canvas.tokens.controlled[0].actor.name
-    // let getLeftItem = item.data.data.qtd
-    // let chatContent = `<h2>Usa Inspiração</h2>` + `<h3>${getActorName} está usando uma @Item[Inspiração].<h3> Resta(m): ${getLeftItem} inspirações. [[/r 1d6]]`
-    //   ChatMessage.create ({
-    //     speaker: {alias: getActorName},
-    //     content: chatContent 
-    //   })
